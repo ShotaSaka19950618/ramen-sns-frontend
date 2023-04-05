3c51659182ab3b4d17f26cdae831324a5d93cd84
Shota.S <geminis218@icloud.com>
2023-04-06 01:37:49 +0900

    deploy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ["ramen-sns-s3.s3.ap-northeast-1.amazonaws.com"],
+    unoptimized: true,
   },
   compiler: {
     styledComponents: true,
